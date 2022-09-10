fdcaf9ab566bed48fd93e012e2f864bb28d780a1
Long Le <longle9669@gmail.com>
2022-09-10 22:09:40 +0700

    Update

---
+++ src/lab_02/lab_22.java
@@ -0,0 +1,17 @@
+package lab_02;
+
+import com.sun.javafx.image.BytePixelSetter;
+
+import java.util.Scanner;
+
+public class lab_22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input the number:");
+        int InputNumber = scanner.nextInt();
+            if (InputNumber % 2 ==0)
+                System.out.println(InputNumber + " is even");
+            else
+                System.out.println(InputNumber + " is odd");
+    }
+}
