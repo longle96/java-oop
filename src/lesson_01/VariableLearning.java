67e37406257a07f66de62a1593364c8cac63621d
unknown <longle9669@gmail.com>
2022-09-10 21:22:58 +0700

    Init

---
+++ src/lesson_01/VariableLearning.java
@@ -0,0 +1,17 @@
+package lesson_01;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        int myIntNum = 18;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        short myShortNum = 18;
+        System.out.println(Short.MAX_VALUE);
+
+        long myLongNum = 18;
+        System.out.println(Long.MAX_VALUE);
+    }
+}
