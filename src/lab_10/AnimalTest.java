f259f862e6e95cde771dbada8cbc6cffd14489f6
Long Le <longle9669@gmail.com>
2022-10-13 19:17:44 +0700

    Update lab_09+10

--- src/lab_10/AnimalTest.java
+++ src/lab_10/AnimalTest.java
@@ -9,8 +9,10 @@ import java.util.List;
 public class AnimalTest {
     public static void main(String[] args) {
         Animal tiger = new Animal.Builder().setName("Tiger").setCanFly(false).setSpeed(new SecureRandom().nextInt(100)).build();
-        Animal cat = new Animal.Builder().setName("Cat").setCanFly(true).setSpeed(new SecureRandom().nextInt(100)).build();
-        List<Animal> animalList = Arrays.asList(cat, tiger);
+        Animal cat = new Animal.Builder().setName("Cat").setCanFly(false).setSpeed(new SecureRandom().nextInt(100)).build();
+        Animal bird = new Animal.Builder().setName("Bird").setCanFly(true).setSpeed(new SecureRandom().nextInt(100)).build();
+        Animal eagle = new Animal.Builder().setName("Eagle").setCanFly(true).setSpeed(100).build();
+        List<Animal> animalList = Arrays.asList(cat,tiger,bird,eagle);
         List<Animal> filterAnimalList = Animal.filterAnimal(animalList);
         String result = Animal.GetWinner(filterAnimalList);
     }
